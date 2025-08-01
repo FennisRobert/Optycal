ebd0b2b18b8657bcfeb56b9732c394a81b2c37db
Robert Fennis <robertfennis@Roberts-MBP.home>
2025-08-01 11:58:12 +0200

    added license headers

--- src/optycal/antennas/patterns.py
+++ src/optycal/antennas/patterns.py
@@ -1,5 +1,19 @@
-from typing import Tuple
+# Optycal is an open source Python based PO Solver.
+# Copyright (C) 2025  Robert Fennis.
 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see
+# <https://www.gnu.org/licenses/>.
 import numpy as np
 from numba import njit, vectorize, f8, c16, f8
 from numba.types import Tuple as TypeTuple
